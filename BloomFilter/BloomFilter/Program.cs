92afca62bef0fb05b1a07250918e72af1b2ecc76
Segger87 <Segger87@gmail.com>
2018-08-09 16:53:27 +0100

    Added an MD5 Hash algorithm Method

---
+++ BloomFilter/BloomFilter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace BloomFilter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var program = new Program();
+			string hash = program.CalculateMD5Hash("Sam");
+			Console.Write(hash);
+			Console.ReadLine();
+		}
+
+
+		private string CalculateMD5Hash(string input)
+		{
+			MD5 md5 = MD5.Create();
+
+			byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+			byte[] hash = md5.ComputeHash(inputBytes);
+
+			StringBuilder stringBuilder = new StringBuilder();
+
+			for (int i = 0; i < hash.Length; i++)
+			{
+				stringBuilder.Append(hash[i].ToString("x2"));
+			}
+
+			return stringBuilder.ToString();
+		}
+	}
+}
