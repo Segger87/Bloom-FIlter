85b01071e56c44f4091039a6499fca72de7d6cc6
Segger87 <Segger87@gmail.com>
2018-08-10 11:40:54 +0100

    Split Bloom filter into its own class to make it cleaner

---
+++ BloomFilter/BloomFilter/BloomFilter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloomFilter
+{
+	public class BloomFilter
+	{
+		public BitArray bitArray = new BitArray(1000, false); 
+
+		public BloomFilter()
+		{
+
+		}
+
+		public BloomFilter(string word)
+		{
+			var hash = CalculateMD5Hash(word);
+			SetValuesInBloomFilter(bitArray, hash);
+		}
+
+		private int[] CalculateMD5Hash(string input)
+		{
+			MD5 md5 = MD5.Create();
+			byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+			byte[] hash = md5.ComputeHash(inputBytes);
+			int[] hashInts = new int[5];
+
+			for (int i = 0; i < 5; i++)
+			{
+				var hashInt = Convert.ToInt16(hash[i]);
+				hashInts[i] = hashInt;
+			}
+
+			return hashInts;
+		}
+
+		private BitArray SetValuesInBloomFilter(BitArray bitArray, int[] input)
+		{
+			foreach (var item in input)
+			{
+				bitArray[item] = true;
+			}
+
+			return bitArray;
+		}
+
+		public bool CheckBloomFilter(string word)
+		{
+			var hash = CalculateMD5Hash(word);
+
+			foreach (var i in hash)
+			{
+				if (!bitArray[i])
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
--- BloomFilter/BloomFilter/Program.cs
+++ BloomFilter/BloomFilter/Program.cs
@@ -12,57 +12,15 @@ namespace BloomFilter
 		static void Main(string[] args)
 		{
 			var program = new Program();
-			BitArray bitArray = new BitArray(1000, false);
 
-			var sam = program.CalculateMD5Hash("Sam");
-			var bob = program.CalculateMD5Hash("Bob");
-
-			bitArray = program.SetValuesInBloomFilter(bitArray, sam);
-			var search = program.CheckBloomFilter(bitArray, sam);
-			var search2 = program.CheckBloomFilter(bitArray, bob);
-
-
-			Console.WriteLine(search);
-			Console.WriteLine(search2);
+			var test = new BloomFilter("Sam");
+			var a = test.CheckBloomFilter("Sam");
+			var test2 = new BloomFilter("Bob");
+			var b = test2.CheckBloomFilter("Bob");
+			
+			Console.WriteLine(a);
+			Console.WriteLine(b);
 			Console.ReadLine();
-		}
-
-		private int[] CalculateMD5Hash(string input)
-		{
-			MD5 md5 = MD5.Create();
-
-			byte[] inputBytes = Encoding.ASCII.GetBytes(input);
-			byte[] hash = md5.ComputeHash(inputBytes);
-			int[] hashInts = new int[5];
-
-			for (int i = 0; i < 5; i++)
-			{
-				var hashInt = Convert.ToInt32(hash[i]);
-					hashInts[i] = hashInt;
-			}
-
-			return hashInts;
-		}
-
-		private BitArray SetValuesInBloomFilter(BitArray bitArray, int[]input)
-		{
-			foreach (var item in input)
-			{
-				bitArray[item] = true;
-			}
-
-			return bitArray;
-		}
-
-		private bool CheckBloomFilter(BitArray bloom, int[] input)
-		{
-			foreach (var item in input)
-			{
-				if(!bloom[item]){
-					return false;
-				}
-			}
-			return true;
 		}
 	}
 }
