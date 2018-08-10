1db32f5dffd9d953dc3de6bdee3126083889c0c1
Segger87 <Segger87@gmail.com>
2018-08-10 11:25:11 +0100

    Implemented basic bloom filter using a BitArray

--- BloomFilter/BloomFilter/Program.cs
+++ BloomFilter/BloomFilter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -10,27 +12,57 @@ namespace BloomFilter
 		static void Main(string[] args)
 		{
 			var program = new Program();
-			string hash = program.CalculateMD5Hash("Sam");
-			Console.Write(hash);
+			BitArray bitArray = new BitArray(1000, false);
+
+			var sam = program.CalculateMD5Hash("Sam");
+			var bob = program.CalculateMD5Hash("Bob");
+
+			bitArray = program.SetValuesInBloomFilter(bitArray, sam);
+			var search = program.CheckBloomFilter(bitArray, sam);
+			var search2 = program.CheckBloomFilter(bitArray, bob);
+
+
+			Console.WriteLine(search);
+			Console.WriteLine(search2);
 			Console.ReadLine();
 		}
 
-
-		private string CalculateMD5Hash(string input)
+		private int[] CalculateMD5Hash(string input)
 		{
 			MD5 md5 = MD5.Create();
 
 			byte[] inputBytes = Encoding.ASCII.GetBytes(input);
 			byte[] hash = md5.ComputeHash(inputBytes);
+			int[] hashInts = new int[5];
 
-			StringBuilder stringBuilder = new StringBuilder();
-
-			for (int i = 0; i < hash.Length; i++)
+			for (int i = 0; i < 5; i++)
 			{
-				stringBuilder.Append(hash[i].ToString("x2"));
+				var hashInt = Convert.ToInt32(hash[i]);
+					hashInts[i] = hashInt;
 			}
 
-			return stringBuilder.ToString();
+			return hashInts;
+		}
+
+		private BitArray SetValuesInBloomFilter(BitArray bitArray, int[]input)
+		{
+			foreach (var item in input)
+			{
+				bitArray[item] = true;
+			}
+
+			return bitArray;
+		}
+
+		private bool CheckBloomFilter(BitArray bloom, int[] input)
+		{
+			foreach (var item in input)
+			{
+				if(!bloom[item]){
+					return false;
+				}
+			}
+			return true;
 		}
 	}
 }
